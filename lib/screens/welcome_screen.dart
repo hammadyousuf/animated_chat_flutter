869790b5bc2e7031c75968866a1c598cc04b2749
hammad <hammadbhat126@gmail.com>
2022-08-28 14:29:20 +0400

    Custom Animations

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,15 +11,46 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+ 
+    super.initState();
+
+    controller = AnimationController(
+        duration: Duration(seconds: 1),
+        vsync: this
+    );
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
+    //animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+    
+    controller.forward();
+
+    controller.addListener(() {
+      setState((){});
+      print(animation.value);
+    });
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        padding: const EdgeInsets.symmetric(horizontal: 7.0),
         child: Column(
+
           mainAxisAlignment: MainAxisAlignment.center,
+
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Row(
@@ -28,15 +59,18 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
 
                   child: Container(
+
                     child: Image.asset('images/logo.png'),
                     height: 60.0,
                   ),
                 ),
                 Text(
-                  'Flash Chat',
-                  style: TextStyle(
+
+                'Flash Chat',
+                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
+                    color: Colors.grey.shade900,
                   ),
                 ),
               ],
